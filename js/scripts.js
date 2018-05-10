32d3c8968f734159a395352752d04864dd15c673
Yaroslav <yaroslavgorpynchuk@gmail.com>
2018-05-10 13:29:51 +0300

    add project Klinica versiya Yarika

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+
+$(function(){
+
+	$('.button-byrger').on('click', function(){
+
+		$('.menu').slideToggle(300, function(){
+
+			if($(this).css('display') === 'none'){
+
+				$(this).removeAttr('style');
+			};
+
+		});
+		
+	});
+
+});
